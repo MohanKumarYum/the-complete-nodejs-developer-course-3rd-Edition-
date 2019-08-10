8c817e7ba9c9a243ebdd9aaec62cc2ff24fa953f
Mohan Kumar <MohanKumarYum@Gmail.com>
2019-08-10 16:07:02 +1000

    Imported validator module S3:E11 completed

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,10 @@
+const validator = require("validator");
+
 const getNotes = require("./notes");
 
 const msg = getNotes();
 
 console.log(msg);
+
+// console.log(validator.isEmail("MohanKumarYum@Gmail.com"));
+console.log(validator.isURL("http:/mead.io"));
