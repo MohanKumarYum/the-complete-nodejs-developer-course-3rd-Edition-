85e8dba2a94ef690797f7cc7ee497f72879b1b1f
Mohan Kumar <MohanKumarYum@Gmail.com>
2019-08-11 21:49:19 +1000

    addNote function added to notes.js, S4:E19 completed

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,48 @@
+const fs = require("fs");
+
+const fileName = "notes.json";
+
 const getNotes = function() {
   return "Your Notes...";
 };
 
-module.exports = getNotes;
+const addNote = function(title, body) {
+  const notes = loadNotes(fileName);
+
+  const duplicateNote = notes.filter(function(note) {
+    return note.title === title;
+  });
+
+  if (duplicateNote.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    });
+    console.log("New note added!");
+  } else {
+    console.log('"Note taken":');
+  }
+
+  saveNotes(notes);
+};
+
+function loadNotes(fileName) {
+  try {
+    const dataBuffer = fs.readFileSync(fileName);
+    const dataJSON = dataBuffer.toString();
+    const data = JSON.parse(dataJSON);
+    return data;
+  } catch (error) {
+    return [];
+  }
+}
+
+function saveNotes(notes) {
+  const noteJSON = JSON.stringify(notes);
+  fs.writeFileSync(fileName, noteJSON);
+}
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote
+};
--- playground/1-json.js
+++ playground/1-json.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const dataBuffer = fs.readFileSync("1-json.json");
 const dataJSON = dataBuffer.toString();
 const user = JSON.parse(dataJSON);
+console.log(typeof user)
 
 user.name = "Mohan";
 user.age = 45;
