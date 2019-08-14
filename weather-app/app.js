66de216e80ffa78e34b922f1c3b25b3cc96feab7
Mohan Kumar <MohanKumarYum@Gmail.com>
2019-08-14 11:23:36 +1000

    Created new folder Weather-app, understanding async in node.js

---
+++ weather-app/app.js
@@ -0,0 +1,5 @@
+console.log("Starting");
+setTimeout(()=>{console.log("1st Timer with 2 Seconds")},2000);
+setTimeout(()=>{console.log("3rd Timer with 0 Seconds")},0);
+setTimeout(()=>{console.log("2nd Timer with 0 Seconds")},0);
+console.log("Ending");
