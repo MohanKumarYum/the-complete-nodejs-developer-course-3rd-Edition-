de45582b07b7561b9766524d48af87ac3809a279
Mohan Kumar <MohanKumarYum@Gmail.com>
2019-08-14 12:22:36 +1000

    Added parameter units to the API and printing daily summary attribute S6:E32 completed

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,12 @@
 const request = require("request");
 
 const url =
-  "https://api.darksky.net/forecast/23ccaeb651d43a606d7d15a6f41414af/37.8267,-122.4233";
+  "https://api.darksky.net/forecast/23ccaeb651d43a606d7d15a6f41414af/37.8267,-122.4233?units=si";
 
 request({ url: url, json: true }, (error, response, body) => {
   console.log(
-    "It is currently " +
+      body.daily.data[0].summary +
+    " It is currently " +
       body.currently.temperature +
       " degrees out. There is " +
       body.currently.precipProbability +
