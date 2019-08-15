d5048d05ce45a81cc927187d73ee642cda0c9d9d
Mohan Kumar <MohanKumarYum@Gmail.com>
2019-08-15 17:58:13 +1000

    Serving html & json via Express Server, S7:E44 completed

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello Express!"));
-app.get("/help", (req, res) => res.send("Help Page"));
-app.get("/about", (req, res) => res.send("About Page"));
-app.get("/weather", (req, res) => res.send("Weather Page"));
+app.get("/", (req, res) => res.send("<h1>Weather</h1>"));
+app.get("/help", (req, res) => res.send([{
+    name: "Mohan"
+},{
+    name: "Sangeetha"
+}
+]));
+app.get("/about", (req, res) => res.send("<h1>About</h1>"));
+app.get("/weather", (req, res) => res.send({
+    forecast: "It is Sunny day.",
+    location: "Bengaluru, Karnataka, India",
+}));
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
