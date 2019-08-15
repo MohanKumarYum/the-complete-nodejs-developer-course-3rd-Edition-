5fe2b0ee1e104eaab78ba4bf0b176e18fabfcdf3
Mohan Kumar <MohanKumarYum@Gmail.com>
2019-08-16 06:12:04 +1000

    Serving up static assets, S7:E45 completed

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,19 +1,35 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("<h1>Weather</h1>"));
-app.get("/help", (req, res) => res.send([{
-    name: "Mohan"
-},{
-    name: "Sangeetha"
-}
-]));
-app.get("/about", (req, res) => res.send("<h1>About</h1>"));
-app.get("/weather", (req, res) => res.send({
-    forecast: "It is Sunny day.",
-    location: "Bengaluru, Karnataka, India",
-}));
+console.log(__dirname);
+console.log(__filename);
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+console.log("publicDirectoryPath:", publicDirectoryPath);
+app.use(express.static(publicDirectoryPath));
+
+// app.get("/help", (req, res) =>
+//   res.send([
+//     {
+//       name: "Mohan"
+//     },
+//     {
+//       name: "Sangeetha"
+//     }
+//   ])
+// );
+
+
+// app.get("/about", (req, res) => res.send("<h1>About</h1>"));
+// app.get("/weather", (req, res) =>
+//   res.send({
+//     forecast: "It is Sunny day.",
+//     location: "Bengaluru, Karnataka, India"
+//   })
+// );
+
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
